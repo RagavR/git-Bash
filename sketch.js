e21608c1d148a37ad12a5c762725c53bfd3a087a
RagavR <ragavreddy20@gmail.com>
2020-11-03 21:37:02 -0500

    bird2 added

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    bird2 = new Bird(200,100);
 }
 
 function draw(){
@@ -58,7 +58,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    bird2.display();
     bird.display();
     platform.display();
 }
